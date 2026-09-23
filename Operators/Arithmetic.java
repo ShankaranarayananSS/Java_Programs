d7626db3734b8041c89d9a1eac65df5bea2a04d4
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-09-23 22:52:20 +0530

    Update Arithmetic Operators

--- Operators/Arithmetic.java
+++ Operators/Arithmetic.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Subscribe{
+public class Arithmetic{
   public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
     System.out.printf("Enter number-1:");
