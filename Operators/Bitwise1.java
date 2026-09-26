6170557d6200c419ae31fec55b6eb9ae1ac5ca50
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-09-26 12:23:44 +0530

    Create Bitwise Operator

---
+++ Operators/Bitwise1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Bitwise1{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter number-1:");
+    int n1 = sc.nextInt();
+    System.out.print("Enter number-2:");
+    int n2 = sc.nextInt();
+    
+    System.out.println(n1 + " & " + n2 + " = " + (n1&n2));
+    System.out.println(n1 + " | " + n2 + " = " + (n1|n2));
+    System.out.println(n1 + " ^ " + n2 + " = " + (n1^n2));
+    System.out.println("~" + n1 + " = " + (~n1));
+    System.out.println("~" + n2 + " = " + (~n2));
+    sc.close();
+  }
+}
